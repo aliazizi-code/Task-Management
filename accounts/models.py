2191ddc4abb0a8c482505c5a654c52286089ec71
aliazizi-code <devaliazizi@gmail.com>
2024-12-26 09:42:14 +0330

    Add custom User model with phone_number field
    - Inherit from AbstractUser to create a custom user model.
    - Add phone_number field with a maximum length of 12.
    - Implement RegexValidator to ensure proper phone number format.

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,14 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    phone_number = models.CharField(
+        max_length=12,
+        blank=False,
+        validators=[
+            RegexValidator(regex=r'^\+?1?\d{9,12}$',
+                           message="Phone number must be entered in the format: '+999999999'. Up to 12 digits allowed.")
+        ],
+    )
