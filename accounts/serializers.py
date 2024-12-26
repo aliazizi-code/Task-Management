512492a268fb9fb815322cef0600a4ed91bf1262
aliazizi-code <devaliazizi@gmail.com>
2024-12-26 09:44:53 +0330

    Add serializers for User and OTP verification
    - Create UserSerializer to handle the user phone_number field.
    - Create VerifyOTPRequestSerializer to validate OTP requests.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,14 @@
 from rest_framework import serializers
 
+from accounts.models import User
 
-class OTPSerializer(serializers.Serializer):
-    otp = serializers.CharField(required=True)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('phone_number',)
+
+
+class VerifyOTPRequestSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(required=True)
+    otp = serializers.IntegerField(required=True)
