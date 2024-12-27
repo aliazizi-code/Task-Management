90e375c1fa6d474db8dfadc4b5a1359801e09feb
aliazizi-code <devaliazizi@gmail.com>
2024-12-27 13:33:25 +0330

    Add slug field to Task model with auto-generation
    - Introduced 'slug' field to the Task model for URL-friendly representation.
    - Implemented custom save method to auto-generate slug from title if not provided or changed.

--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,6 @@
 from django.core.validators import RegexValidator
 from django.db import models
+from django.utils.text import slugify
 
 
 class Tag(models.Model):
@@ -25,11 +26,17 @@ class Task(models.Model):
         cancelled = 'Cancelled'
 
     title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
     status = models.CharField(choices=Status.choices, default=Status.in_progress, max_length=100)
     tags = models.ManyToManyField(Tag, blank=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug or self.slug != slugify(self.title):
+            self.slug = slugify(self.title)
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
