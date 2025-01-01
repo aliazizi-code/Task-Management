42938110a28ab7009a1dce615ca3a939f79ab478
aliazizi-code <devaliazizi@gmail.com>
2025-01-01 15:39:15 +0330

    Extend User model to include phone number and custom username field
    - Inherited from AbstractUser to create a custom User model.
    - Added phone_number field with unique constraint and validation for format.
    - Made username field nullable and optional.
    - Set USERNAME_FIELD to phone_number, making it the primary identifier.
    - Specified REQUIRED_FIELDS to include username for user creation.

--- accounts/models.py
+++ accounts/models.py
@@ -6,9 +6,18 @@ from django.db import models
 class User(AbstractUser):
     phone_number = models.CharField(
         max_length=12,
+        unique=True,
         blank=False,
         validators=[
-            RegexValidator(regex=r'^\+?1?\d{9,12}$',
-                           message="Phone number must be entered in the format: '+999999999'. Up to 12 digits allowed.")
+            RegexValidator(
+                regex=r'^\+?1?\d{9,12}$',
+                message="Phone number must be entered in the format: '+999999999'. Up to 12 digits allowed.")
         ],
     )
+    username = models.CharField(unique=False, null=True, blank=True)
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.phone_number
