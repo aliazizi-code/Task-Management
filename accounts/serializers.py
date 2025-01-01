c79e45a511d8d7f6ec9277f69dabc8ad724c397b
aliazizi-code <devaliazizi@gmail.com>
2025-01-01 15:40:43 +0330

    Implement UserSerializer and VerifyOTPRequestSerializer for phone number validation
    - Created UserSerializer for validating phone numbers with custom RegexValidator.
    - Added VerifyOTPRequestSerializer to handle OTP verification requests.
    - Both serializers enforce phone number format and length constraints.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,14 +1,27 @@
+from django.core.validators import RegexValidator
 from rest_framework import serializers
 
-from accounts.models import User
 
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('phone_number',)
+class UserSerializer(serializers.Serializer):
+    phone_number = serializers.CharField(
+        max_length=12,
+        validators=[
+            RegexValidator(
+                regex=r'^\+?1?\d{9,12}$',
+                message="Phone number must be entered in the format: '+999999999'. Up to 12 digits allowed."
+            )
+        ],
+    )
 
 
 class VerifyOTPRequestSerializer(serializers.Serializer):
-    phone_number = serializers.CharField(required=True)
+    phone_number = serializers.CharField(
+        max_length=12,
+        validators=[
+            RegexValidator(
+                regex=r'^\+?1?\d{9,12}$',
+                message="Phone number must be entered in the format: '+999999999'. Up to 12 digits allowed."
+            )
+        ],
+    )
     otp = serializers.IntegerField(required=True)
