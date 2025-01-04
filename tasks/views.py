05474b24668bdc2e54a3241726187e4c07b2be5f
aliazizi-code <devaliazizi@gmail.com>
2025-01-04 18:45:12 +0330

    Implement TaskViewSet with pagination and authentication
    - Created ListPagination class for paginating Task results.
    - Implemented TaskViewSet with list and create methods.
    - Added pagination and authentication for listing tasks.

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
 
-# Create your views here.
+from tasks.models import Task
+from tasks.serializers import TaskSerializer
+
+
+class ListPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'list_count': self.page.paginator.count,
+            'results': data
+        })
+
+
+class TaskViewSet(ViewSet):
+    pagination_class = ListPagination
+    permission_classes = (IsAuthenticated,)
+
+    def list(self, request):
+        queryset = Task.objects.all()
+
+        paginator = self.pagination_class()
+        page = paginator.paginate_queryset(queryset, request)
+        if page is not None:
+            serializer = TaskSerializer(page, many=True)
+            return paginator.get_paginated_response(serializer.data)
+
+        serializer = TaskSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = TaskSerializer(data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save(user=request.user)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
