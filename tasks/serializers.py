0203efc98a9f78fcd64ab810e8de7a9b3e20b46d
aliazizi-code <devaliazizi@gmail.com>
2025-01-04 18:43:04 +0330

    Add TaskSerializer for Task model
    - Implemented TaskSerializer to serialize Task data.
    - Added custom create method to handle tags as a comma-separated string.
    - Ensured tags are created or retrieved and associated with the Task.

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,10 +1,23 @@
 from rest_framework import serializers
 
-from tasks.models import Task
+from tasks.models import Task, Tag
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    tags = serializers.CharField(required=False)
+
     class Meta:
         model = Task
-        fields = '__all__'
-        read_only = ('created_date', 'updated_date', 'slug')
+        fields = ['title', 'description', 'status', 'tags']
+
+    def create(self, validated_data):
+        tags_data = validated_data.pop('tags', None)
+        task = Task.objects.create(**validated_data)
+
+        if tags_data:
+            tags_names = [name.strip() for name in tags_data.split(',')]
+            for tag_name in tags_names:
+                tag, created = Tag.objects.get_or_create(name=tag_name)
+                task.tags.add(tag)
+
+        return task
