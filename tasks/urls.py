6b1209dbca0eb8da27d0a81ca23e43b5507a5ea4
aliazizi-code <devaliazizi@gmail.com>
2025-01-05 17:14:42 +0330

    Add update endpoint for TaskViewSet
    - Defined URL path for partially updating tasks using slug.
    - Integrated 'partial_update' method in TaskViewSet for handling patch requests.

--- tasks/urls.py
+++ tasks/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.TaskViewSet.as_view({'get': 'list'}), name='list'),
     path('create/', views.TaskViewSet.as_view({'post': 'create'}), name='create'),
     path('detail/<slug:slug>/', views.TaskViewSet.as_view({'get': 'retrieve'}), name='detail'),
+    path('update/<slug:slug>/', views.TaskViewSet.as_view({'patch': 'partial_update'}), name='update'),
 ]
