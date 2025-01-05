612dc4c1200d0fab8beb9313cfed0b1bb1e7485c
aliazizi-code <devaliazizi@gmail.com>
2025-01-05 17:26:19 +0330

    path('delete/<slug:slug>/', views.TaskViewSet.as_view({'delete': 'destroy'}), name='delete'),

--- tasks/urls.py
+++ tasks/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('create/', views.TaskViewSet.as_view({'post': 'create'}), name='create'),
     path('detail/<slug:slug>/', views.TaskViewSet.as_view({'get': 'retrieve'}), name='detail'),
     path('update/<slug:slug>/', views.TaskViewSet.as_view({'patch': 'partial_update'}), name='update'),
+    path('delete/<slug:slug>/', views.TaskViewSet.as_view({'delete': 'destroy'}), name='delete'),
 ]
