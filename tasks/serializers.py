17e165792c0429a07d768b677f1f4f90d91d828d
aliazizi-code <devaliazizi@gmail.com>
2025-01-05 17:14:09 +0330

    Add update method to TaskCreateUpdateSerializer
    - Implemented update method to handle task updates, allowing tag modification.
    - Added logic to clear existing tags and add new tags during the update process.

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from tasks.models import Task, Tag
 
 
-class CreateTaskSerializer(serializers.ModelSerializer):
+class TaskCreateUpdateSerializer(serializers.ModelSerializer):
     tags = serializers.CharField(required=False)
 
     class Meta:
@@ -21,6 +21,23 @@ class CreateTaskSerializer(serializers.ModelSerializer):
                 task.tags.add(tag)
 
         return task
+
+    def update(self, instance, validated_data):
+        tags_data = validated_data.pop('tags', None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        if tags_data is not None:
+            tags_names = [name.strip() for name in tags_data.split(',')]
+            instance.tags.clear()
+            for tag_name in tags_names:
+                tag, created = Tag.objects.get_or_create(name=tag_name)
+                instance.tags.add(tag)
+
+        return instance
+
 
 class ResponseTaskSerializer(serializers.ModelSerializer):
     tags = serializers.StringRelatedField(many=True)
