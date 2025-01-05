999b875145d8ee402aca9720e8ac306705b0ebf6
aliazizi-code <devaliazizi@gmail.com>
2025-01-05 13:21:25 +0330

    Add URL path for Task detail view
    - Defined path for retrieving Task detail by slug.

--- tasks/urls.py
+++ tasks/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.TaskViewSet.as_view({'get': 'list'}), name='list'),
     path('create/', views.TaskViewSet.as_view({'post': 'create'}), name='create'),
+    path('detail/<slug:slug>/', views.TaskViewSet.as_view({'get': 'retrieve'}), name='detail'),
 ]
